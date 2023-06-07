aa823a83a9dbb94ff466581a7cbd7ffac34f77cd
shahar cohen <shahar9840@gmail.com>
2023-06-07 17:16:28 +0300

    menufront last commit

--- src/Pages/Menu.js
+++ src/Pages/Menu.js
@@ -6,6 +6,7 @@ import './Menu.css'
 
 function Menu({categories}) {
 const [dishes,setDishes]=React.useState([])
+const [categoryName,setCategoryName]=React.useState('')
 
   
   return (
@@ -15,7 +16,7 @@ const [dishes,setDishes]=React.useState([])
       <div style={{backgroundColor:"#F5FFFA" ,borderRadius:'3px',boxShadow:"0px 2px 10px #D3D3D3"}}>
       {
         categories.map((category)=>(
-        <div onClick={()=>(setDishes(category.dishes))} style={{border:"1px solid lightGray",margin:'5px',borderRadius:'3px',pointerEvents:"fill"}}>
+        <div onClick={()=>(setDishes(category.dishes),setCategoryName(category.name))} style={{border:"1px solid lightGray",margin:'5px',borderRadius:'3px',pointerEvents:"fill"}}>
               <img style={{width:"200px",height:'200px',margin:'10px', borderRadius:'3px'}} src={category.imageUrl}/>
               <div><h3>{category.name}</h3></div>
         </div>
@@ -23,7 +24,7 @@ const [dishes,setDishes]=React.useState([])
       }
           </div>
           <div>
-            <div><h1>שם קטגוריה</h1></div>
+            <div><h1>{categoryName}</h1></div>
             <div style={{display:'flex',flexWrap: "wrap",margin:'5px'}}>
           {
             dishes.map((dish)=>(
